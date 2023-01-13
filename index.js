171b801c6a5b4d2428a12c0c0390ebae37d6d0ef
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2023-01-13 09:13:10 +0100

    check passwd

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ window.addEventListener('DOMContentLoaded', main)
 
 function main() {
     const form = document.getElementById('form')
+    const passwdInput = document.getElementById('contraseña')
+    const repeatPasswdInput = document.getElementById('repeat-contraseña')
 
     form.addEventListener('submit', handleSubmit)
 
@@ -10,5 +12,9 @@ function main() {
      */
     function handleSubmit (e) {
         e.preventDefault()
+        
+        if (passwdInput.value === repeatPasswdInput.value) {
+            form.submit()
+        }
     }
 }
