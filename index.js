02683691c76d0bbeb6b7db7f1b05fc2a3cc5b150
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2023-01-13 09:58:19 +0100

    phone prevent non-numeric input

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ function main() {
     const passwdInput = document.getElementById('contraseña')
     /** @type {HTMLInputElement} */
     const repeatPasswdInput = document.getElementById('repeat-contraseña')
+    /** @type {HTMLInputElement} */
+    const phoneInput = document.getElementById('telefono')
 
     form.addEventListener('submit', handleSubmit)
+    phoneInput.addEventListener('keypress', handlePhoneInput)
     passwdInput.addEventListener('input', handlePasswdInput)
     passwdInput.addEventListener('change', handlePasswdInput)
     repeatPasswdInput.addEventListener('input', handlePasswdEqualtoRepeatPasswd)
     repeatPasswdInput.addEventListener('change', handlePasswdEqualtoRepeatPasswd)
+    
+    /**
+     * 
+     * @param {KeyboardEvent} e 
+     */
+    function handlePhoneInput (e) {
+        const n = Number(e.key)
+
+        if (e.key === ' ' || Number.isNaN(n)) {
+            e.preventDefault()
+        }
+    }
     
     /**
      * @param {SubmitEvent} e
